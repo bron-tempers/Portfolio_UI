f8eae1a35066e5dda79028f13f41bf476c60591d
Bron Tempers <bron.tempers@gmail.com>
2021-10-03 17:18:38 +0300

    finish header add responsiv nav-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 "use strict";
 
 /* burger menu */
+const burgerMenu = document.querySelector(".nav-menu"),
+      burgerButton = document.querySelector(".menu-button"),
+      burgerButtonLines = document.querySelectorAll(".menu-button__line");
+
+if (burgerButton) {
+    burgerButton.addEventListener("click", () => {
+        burgerMenu.classList.toggle("nav-menu--active");
+        for (let i = 0; i < burgerButtonLines.length; i++) {
+            burgerButtonLines[i].classList.toggle("menu-button__line--active")
+        }
+        document.body.classList.toggle("body--lock");
+    })
+}
